bdc427ff008a0261fa44ebe3948c5d0a33e82353
Rafa Hady Heryanto <rafahady101@gmail.com>
2021-04-03 23:02:19 +0700

    Update prog_butik.py

--- prog_butik.py
+++ prog_butik.py
@@ -1,7 +1,7 @@
 print(7*"<","SELAMAT DATANG DI PROGRAM BUTIK",">"*7)
-print("1. Kaos")
-print("2. Kemeja")
-print("3. Jas")
+print("1. Kaos polos (Rp25.000,00)")
+print("2. Kemeja (Rp50.000,00)")
+print("3. Jas (Rp100,000.00)")
 print("4. Selesai")
 kaos = 25000
 kemeja = 50000
@@ -10,7 +10,7 @@ total = 0
 while True:
     beli = str(input("Masukkan barang yang ingin dibeli (1/2/3/4): "))
     if beli == "1" or beli == "kaos" or beli == "Kaos":
-        print("Kaos ditambahkan")
+        print("Kaos polos ditambahkan")
         total += kaos
     elif beli == "2" or beli == "kemeja" or beli == "Kemeja":
         print("Kemeja ditambahkan")
@@ -19,16 +19,19 @@ while True:
         print("Jas ditambahkan")
         total += jas
     elif beli == "4" or beli == "selesai" or beli == "Selesai" :
-        print("Total yang harus dibayar:",total)
+        if total == 0:
+            break
+        print("\nTotal yang harus dibayar:",total)
         break
     else:
         print("Inputan anda salah, opsi yang tersedia adalah (1/2/3/4)")
-bayar = int(input("Masukkan uang anda: "))
-if bayar == total:
-    print("Uang anda sudah pas, Terima kasih sudah belanja!")
-elif bayar > total:
-    kembalian = bayar - total
-    print("Kembalian anda sebesar",kembalian,"Terima kasih sudah belanja!")
-elif bayar < total:
-    kurang = total - bayar
-    print("Mohon maaf uang anda kurang sebesar",kurang)
+if total != 0:
+    bayar = int(input("Masukkan uang anda: "))
+    if bayar == total:
+       print("Uang anda sudah pas, Terima kasih sudah belanja!")
+    elif bayar > total:
+       kembalian = bayar - total
+       print("Kembalian anda sebesar",kembalian,"Terima kasih sudah belanja!")
+    elif bayar < total:
+       kurang = total - bayar
+       print("Mohon maaf uang anda kurang sebesar",kurang)
