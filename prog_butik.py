33a6a2542009bc89b79e1b80813234553ce1a912
Rafa Hady Heryanto <rafahady101@gmail.com>
2021-04-03 19:09:39 +0700

    Create prog_butik.py

---
+++ prog_butik.py
@@ -0,0 +1,25 @@
+print(7*"<","SELAMAT DATANG DI PROGRAM BUTIK",">"*7)
+print("1. Kaos")
+print("2. Kemeja")
+print("3. Jas")
+print("4. Selesai")
+kaos = 25000
+kemeja = 50000
+jas = 100000
+total = 0
+while True:
+    beli = str(input("Masukkan barang yang ingin dibeli (1/2/3/4): "))
+    if beli == "1" or beli == "kaos" or beli == "Kaos":
+        print("Kaos ditambahkan")
+        total += kaos
+    elif beli == "2" or beli == "kemeja" or beli == "Kemeja":
+        print("Kemeja ditambahkan")
+        total += kemeja
+    elif beli == "3" or beli == "jas" or beli == "Jas":
+        print("Jas ditambahkan")
+        total += jas
+    elif beli == "4" or beli == "selesai" or beli == "Selesai" :
+        print("Total yang harus dibayar:",total)
+        break
+    else:
+        print("Inputan anda salah, opsi yang tersedia adalah (1/2/3/4)")
